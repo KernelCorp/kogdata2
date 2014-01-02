c331df9a33ed0fa02dd079c98e66d803ac9df012
max-konin <mk@kernel-corp.com>
2014-01-02 22:41:37 +0700

    init rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kogdata2::Application.config.secret_key_base = '2cf2e43d182a37e1f52d5c962b426c6f24191a508570a1c107b3b879315f2f494be5f1e1363ba7c83ae1a0a68ac9bf7c460b0b73dabf226e0640695a2969a098'
