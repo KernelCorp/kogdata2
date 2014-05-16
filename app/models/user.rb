1cea3eb435c5b92ff14c2b343547145eb16aca5b
Dmitry Maganov <vonagam@gmail.com>
2014-05-16 15:51:15 +0700

    finish devise enter by email or login

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: :login_or_email
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login_or_email]
 
   ## Database authenticatable
   field :email,              type: String, default: ""
