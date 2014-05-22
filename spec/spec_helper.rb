f2a122cd170b31b76ca114e71c7eb1875f2bfaa7
max <mk@kernel-corp.com>
2014-05-22 12:28:48 +0700

    coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,9 @@ require 'rspec'
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'database_cleaner'
+require 'coveralls'
+
+Coveralls.wear!('rails')
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
