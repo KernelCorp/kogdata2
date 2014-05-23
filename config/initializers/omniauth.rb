068363d36965c9df1eb79fb70ea0fd4f127bee40
Dmitry Maganov <vonagam@gmail.com>
2014-05-23 14:03:22 +0700

    vk on production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
 
-  provider :vkontakte, '4251492', 'iOmxFio77a9DgrFNguoA'#, scope: 'email' 
+  if Rails.env.production?
+    provider :vkontakte, '3667679', 'nv4OpLwMmAL0JVznk8YR'
+  else
+    provider :vkontakte, '4251492', 'iOmxFio77a9DgrFNguoA'
+  end
 end
